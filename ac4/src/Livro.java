035b6e1b38ed2e88e40f79d53d39a17cbf8ef974
Luahn <115737683+LuahnKaye@users.noreply.github.com>
2023-09-10 23:52:47 -0300

    Add files via upload

---
+++ ac4/src/Livro.java
@@ -0,0 +1,13 @@
+public abstract class Livro {
+    String nome;
+    String autor;
+
+    public Livro(String nome, String autor) {
+        this.nome = nome;
+        this.autor = autor;
+    }
+
+public abstract void reservar();
+
+public abstract void cancelarReserva();
+}
